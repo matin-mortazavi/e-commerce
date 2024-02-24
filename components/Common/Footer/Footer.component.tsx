71dce835bf773a6518ac2cad88c89dbfecba5393
matin.mortazavi.a <matin.mortazavi.a@gmail.com>
2024-02-24 16:12:16 +0330

    feat (home) : footer added

--- components/Common/Footer/Footer.component.tsx
+++ components/Common/Footer/Footer.component.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+// Footer.tsx
+import React from 'react';
+import styles from './Footer.module.scss';
 
-const Footer = () => {
-  return <div>Footer</div>;
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.logo}>Your Logo</div>
+        <div className={styles.info}>
+          <p>&copy; 2024 Your Company. All rights reserved.</p>
+          <p>Contact us: info@example.com</p>
+        </div>
+        <div className={styles.social}>
+          <a href="#" className={styles.socialIcon}>
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="#" className={styles.socialIcon}>
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className={styles.socialIcon}>
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
